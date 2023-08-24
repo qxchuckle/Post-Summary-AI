965d941602f21b20bdc5f8e299ae0420b09cf9bd
qxchuckle <1934009145@qq.com>
2023-08-25 01:45:43 +0800

    新增切换简介功能及相关配置

--- chuckle-post-ai.js
+++ chuckle-post-ai.js
@@ -53,6 +53,7 @@ function ChucklePostAI(AI_option) {
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21px" height="21px" viewBox="0 0 48 48">
         <g id="&#x673A;&#x5668;&#x4EBA;" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><path d="M34.717885,5.03561087 C36.12744,5.27055371 37.079755,6.60373651 36.84481,8.0132786 L35.7944,14.3153359 L38.375,14.3153359 C43.138415,14.3153359 47,18.1768855 47,22.9402569 L47,34.4401516 C47,39.203523 43.138415,43.0650727 38.375,43.0650727 L9.625,43.0650727 C4.861585,43.0650727 1,39.203523 1,34.4401516 L1,22.9402569 C1,18.1768855 4.861585,14.3153359 9.625,14.3153359 L12.2056,14.3153359 L11.15519,8.0132786 C10.920245,6.60373651 11.87256,5.27055371 13.282115,5.03561087 C14.69167,4.80066802 16.024865,5.7529743 16.25981,7.16251639 L17.40981,14.0624532 C17.423955,14.1470924 17.43373,14.2315017 17.43948,14.3153359 L30.56052,14.3153359 C30.56627,14.2313867 30.576045,14.1470924 30.59019,14.0624532 L31.74019,7.16251639 C31.975135,5.7529743 33.30833,4.80066802 34.717885,5.03561087 Z M38.375,19.4902885 L9.625,19.4902885 C7.719565,19.4902885 6.175,21.0348394 6.175,22.9402569 L6.175,34.4401516 C6.175,36.3455692 7.719565,37.89012 9.625,37.89012 L38.375,37.89012 C40.280435,37.89012 41.825,36.3455692 41.825,34.4401516 L41.825,22.9402569 C41.825,21.0348394 40.280435,19.4902885 38.375,19.4902885 Z M14.8575,23.802749 C16.28649,23.802749 17.445,24.9612484 17.445,26.3902253 L17.445,28.6902043 C17.445,30.1191812 16.28649,31.2776806 14.8575,31.2776806 C13.42851,31.2776806 12.27,30.1191812 12.27,28.6902043 L12.27,26.3902253 C12.27,24.9612484 13.42851,23.802749 14.8575,23.802749 Z M33.1425,23.802749 C34.57149,23.802749 35.73,24.9612484 35.73,26.3902253 L35.73,28.6902043 C35.73,30.1191812 34.57149,31.2776806 33.1425,31.2776806 C31.71351,31.2776806 30.555,30.1191812 30.555,28.6902043 L30.555,26.3902253 C30.555,24.9612484 31.71351,23.802749 33.1425,23.802749 Z" id="&#x5F62;&#x72B6;&#x7ED3;&#x5408;" fill="#444444" fill-rule="nonzero"></path></g></svg>
         <div class="ai-title-text">${interface.name}</div>
+        <div class="ai-Toggle">切换简介</div>
         <div class="ai-tag">${interface.version}</div>
       </div>
       <div class="ai-explanation">${interface.name}初始化中...</div>
@@ -68,6 +69,7 @@ function ChucklePostAI(AI_option) {
     let explanation = document.querySelector('.ai-explanation');
     let post_ai = document.querySelector('.post-ai');
     let ai_btn_item = document.querySelectorAll('.ai-btn-item');
+    let ai_toggle = document.querySelector('.ai-Toggle');
     let ai_str = '';
     let ai_str_length = '';
     let delay_init = 600;
@@ -81,6 +83,10 @@ function ChucklePostAI(AI_option) {
     let controller = new AbortController();//控制fetch
     let signal = controller.signal;
     let visitorId = ""; // 标识访客ID
+    const summary_toggle = AI_option.summary_toggle ?? true;
+    let switch_control = 0;
+    let executedForSwitchControl = false;
+    const summary_num = AI_option.summary_num || 3; // 切换时允许生成的摘要总数，默认3个
     //默认true，使用tianliGPT，false使用官方api，记得配置Key
     const choiceApi = true;
     const apiKey = "填入chatGPT的apiKey";
@@ -164,6 +170,10 @@ function ChucklePostAI(AI_option) {
       }
       ai_str = '';
       ai_str_length = '';
+      if(summary_toggle){
+        ai_toggle.style.opacity = "0";
+        ai_toggle.style.pointerEvents = "none";
+      }
       observer.disconnect();// 暂停上一次监听
     }
     function startAI(str, df = true) {
@@ -193,11 +203,43 @@ function ChucklePostAI(AI_option) {
     }
     async function aiGenerateAbstract() {
       resetAI();
-      const ele = targetElement
+      const ele = targetElement;
       const content = getTextContent(ele);
       const response = await getGptResponse(content, choiceApi);//true使用tianliGPT，false使用官方api
       if(response){
         startAI(response);
+        if(summary_toggle){
+          ai_toggle.style.opacity = "1";
+          ai_toggle.style.pointerEvents = "auto";
+        }
+      }
+    }
+    async function switchAbstract() {
+      resetAI();
+      const ele = targetElement;
+      switch_control = (switch_control + 1) % summary_num;
+      const content = getTextContent(ele) + "#".repeat(switch_control);
+      let response = "";
+      if(switch_control === 1 && !executedForSwitchControl){
+        sessionStorage.setItem('backupsSummary', sessionStorage.getItem('summary')); // 将第一次的简介存起来
+        executedForSwitchControl = true;
+      }
+      if(!sessionStorage.getItem(`summary${"#".repeat(switch_control)}`)){
+        sessionStorage.removeItem('summary');
+        response = await getGptResponse(content, choiceApi);
+        sessionStorage.setItem(`summary${"#".repeat(switch_control)}`, response);
+      }else{
+        response = sessionStorage.getItem(`summary${"#".repeat(switch_control)}`);
+        if(switch_control === 0){
+          sessionStorage.setItem('summary', sessionStorage.getItem('backupsSummary'));
+        }else{
+          sessionStorage.setItem('summary', sessionStorage.getItem(`summary${"#".repeat(switch_control)}`));
+        }
+      }
+      if(response){
+        startAI(response);
+        ai_toggle.style.opacity = "1";
+        ai_toggle.style.pointerEvents = "auto";
       }
     }
     async function recommendList() {
@@ -319,7 +361,10 @@ function ChucklePostAI(AI_option) {
     async function ai_init() {
       // 清除缓存
       sessionStorage.removeItem('recommendList');
-      sessionStorage.removeItem('summary');
+      sessionStorage.removeItem('backupsSummary');
+      for (let i = 0; i < summary_num; i++) {
+        sessionStorage.removeItem(`summary${"#".repeat(i)}`);
+      }
       explanation = document.querySelector('.ai-explanation');
       post_ai = document.querySelector('.post-ai');
       ai_btn_item = document.querySelectorAll('.ai-btn-item');
@@ -333,6 +378,14 @@ function ChucklePostAI(AI_option) {
           funArr[index]();
         });
       });
+      ai_toggle = document.querySelector('.ai-Toggle');
+      if(summary_toggle){
+        ai_toggle.addEventListener('click', () => {
+          switchAbstract();
+        });
+      }else{
+        ai_toggle.style.display = 'none';
+      }
       if(AI_option.summary_directly){
         aiGenerateAbstract();
       }else{
@@ -428,7 +481,7 @@ function ChucklePostAI(AI_option) {
     //发送请求获得简介
     async function getGptResponse(content, i = true) {
       if (!tlKey) {
-        return "没有获取到key，代码可能没有安装正确。如果你需要在tianli_gpt文件引用前定义tianliGPT_key变量。详细请查看文档。";
+        return "没有获取到key，代码可能没有安装正确，详细请查看文档。";
       }
       if (tlKey === "123456") {
         return "请购买 key 使用，如果你能看到此条内容，则说明代码安装正确。";
@@ -593,14 +646,14 @@ function ChucklePostAI(AI_option) {
       // 返回文章内容所在的容器元素
       return findArticleContentElement();
     }
-
+    
     // 插入css
     function insertCSS(){
       const styleId = 'qx-ai-style';
       if(document.getElementById(styleId)) { return; }
       const styleElement = document.createElement('style');
       styleElement.id = styleId;
-      styleElement.textContent = AI_option.css || `:root{--ai-font-color:#353535;--ai-post-bg:#f1f3f8;--ai-content-bg:#fff;--ai-content-border:1px solid #e3e8f7;--ai-border:1px solid #e3e8f7bd;--ai-tag-bg:rgba(48,52,63,0.80);--ai-cursor:#333;--ai-btn-bg:rgba(48,52,63,0.75);--ai-title-color:#4c4948;--ai-btn-color:#fff;}[data-theme=dark],.theme-dark,body.dark,body.dark-theme{--ai-font-color:rgba(255,255,255,0.9);--ai-post-bg:#30343f;--ai-content-bg:#1d1e22;--ai-content-border:1px solid #42444a;--ai-border:1px solid #3d3d3f;--ai-tag-bg:#1d1e22;--ai-cursor:rgb(255,255,255,0.9);--ai-btn-bg:#1d1e22;--ai-title-color:rgba(255,255,255,0.86);--ai-btn-color:rgb(255,255,255,0.9);}#post-ai.post-ai{background:var(--ai-post-bg);border-radius:12px;padding:10px 12px 11px;line-height:1.3;border:var(--ai-border);margin-top:10px;margin-bottom:6px;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-title{display:flex;color:var(--ai-title-color);border-radius:8px;align-items:center;padding:0 6px;}#post-ai .ai-title i{font-weight:800;}#post-ai .ai-title-text{font-weight:bold;margin-left:8px;}#post-ai .ai-tag{font-size:12px;background-color:var(--ai-tag-bg);color:var(--ai-btn-color);border-radius:4px;margin-left:auto;line-height:1;padding:4px 5px;border:var(--ai-border);}#post-ai .ai-explanation{margin-top:10px;padding:8px 12px;background:var(--ai-content-bg);border-radius:8px;border:var(--ai-content-border);font-size:15.5px;line-height:1.4;color:var(--ai-font-color);}#post-ai .ai-cursor{display:inline-block;width:7px;background:var(--ai-cursor);height:16px;margin-bottom:-2px;opacity:0.95;margin-left:3px;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-btn-box{font-size:15.5px;width:100%;display:flex;flex-direction:row;flex-wrap:wrap;}#post-ai .ai-btn-item{padding:5px 10px;margin:10px 16px 0px 5px;width:fit-content;line-height:1;background:var(--ai-btn-bg);border:var(--ai-border);color:var(--ai-btn-color);border-radius:6px 6px 6px 0;-webkit-border-radius:6px 6px 6px 0;-moz-border-radius:6px 6px 6px 0;-ms-border-radius:6px 6px 6px 0;-o-border-radius:6px 6px 6px 0;user-select:none;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-btn-item:hover{background:#49b0f5dc;}#post-ai .ai-recommend{display:flex;flex-direction:row;flex-wrap:wrap;}#post-ai .ai-recommend-item{width:50%;margin-top:2px;}#post-ai .ai-recommend-item a{border-bottom:2px solid #4c98f7;padding:0 .2em;color:#4c98f7;font-weight:700;text-decoration:none;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-recommend-item a:hover{background-color:#49b1f5;border-bottom:2px solid #49b1f5;color:#fff;border-radius:5px;}@media screen and (max-width:768px){#post-ai .ai-btn-box{justify-content:center;}}#post-ai .ai-title>svg{width:21px;height:fit-content;}#post-ai .ai-title>svg path{fill:var(--ai-font-color);}`;
+      styleElement.textContent = AI_option.css || `:root{--ai-font-color:#353535;--ai-post-bg:#f1f3f8;--ai-content-bg:#fff;--ai-content-border:1px solid #e3e8f7;--ai-border:1px solid #e3e8f7bd;--ai-tag-bg:rgba(48,52,63,0.80);--ai-cursor:#333;--ai-btn-bg:rgba(48,52,63,0.75);--ai-title-color:#4c4948;--ai-btn-color:#fff;}[data-theme=dark],.theme-dark,body.dark,body.dark-theme{--ai-font-color:rgba(255,255,255,0.9);--ai-post-bg:#30343f;--ai-content-bg:#1d1e22;--ai-content-border:1px solid #42444a;--ai-border:1px solid #3d3d3f;--ai-tag-bg:#1d1e22;--ai-cursor:rgb(255,255,255,0.9);--ai-btn-bg:#1d1e22;--ai-title-color:rgba(255,255,255,0.86);--ai-btn-color:rgb(255,255,255,0.9);}#post-ai.post-ai{background:var(--ai-post-bg);border-radius:12px;padding:10px 12px 11px;line-height:1.3;border:var(--ai-border);margin-top:10px;margin-bottom:6px;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-title{display:flex;color:var(--ai-title-color);border-radius:8px;align-items:center;padding:0 6px;}#post-ai .ai-title i{font-weight:800;}#post-ai .ai-title-text{font-weight:bold;margin-left:8px;}#post-ai .ai-tag{font-size:12px;background-color:var(--ai-tag-bg);color:var(--ai-btn-color);border-radius:4px;margin-left:auto;line-height:1;padding:4px 5px;border:var(--ai-border);}#post-ai .ai-explanation{margin-top:10px;padding:8px 12px;background:var(--ai-content-bg);border-radius:8px;border:var(--ai-content-border);font-size:15.5px;line-height:1.4;color:var(--ai-font-color);}#post-ai .ai-cursor{display:inline-block;width:7px;background:var(--ai-cursor);height:16px;margin-bottom:-2px;opacity:0.95;margin-left:3px;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-btn-box{font-size:15.5px;width:100%;display:flex;flex-direction:row;flex-wrap:wrap;}#post-ai .ai-btn-item{padding:5px 10px;margin:10px 16px 0px 5px;width:fit-content;line-height:1;background:var(--ai-btn-bg);border:var(--ai-border);color:var(--ai-btn-color);border-radius:6px 6px 6px 0;-webkit-border-radius:6px 6px 6px 0;-moz-border-radius:6px 6px 6px 0;-ms-border-radius:6px 6px 6px 0;-o-border-radius:6px 6px 6px 0;user-select:none;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-btn-item:hover{background:#49b0f5dc;}#post-ai .ai-recommend{display:flex;flex-direction:row;flex-wrap:wrap;}#post-ai .ai-recommend-item{width:50%;margin-top:2px;}#post-ai .ai-recommend-item a{border-bottom:2px solid #4c98f7;padding:0 .2em;color:#4c98f7;font-weight:700;text-decoration:none;transition:all 0.3s;-webkit-transition:all 0.3s;-moz-transition:all 0.3s;-ms-transition:all 0.3s;-o-transition:all 0.3s;}#post-ai .ai-recommend-item a:hover{background-color:#49b1f5;border-bottom:2px solid #49b1f5;color:#fff;border-radius:5px;}@media screen and (max-width:768px){#post-ai .ai-btn-box{justify-content:center;}}#post-ai .ai-title>svg{width:21px;height:fit-content;}#post-ai .ai-title>svg path{fill:var(--ai-font-color);}#post-ai .ai-Toggle{font-size:12px;border:var(--ai-border);background:var(--ai-btn-bg);color:var(--ai-btn-color);padding:3px 4px;border-radius:4px;margin-left:6px;cursor:pointer;-webkit-transition:.3s;-moz-transition:.3s;-o-transition:.3s;-ms-transition:.3s;transition:.3s;font-weight:bolder;pointer-events:none;opacity:0;}#post-ai .ai-Toggle:hover{background:#49b0f5dc;}`;
       AI_option.additional_css && (styleElement.textContent += AI_option.additional_css);
       document.head.appendChild(styleElement);
     }
